e075d0c11da092df2f4368666b4f49783697f737
Linisdjxm <35957435+Linisdjxm@users.noreply.github.com>
2021-02-16 09:54:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import platform;
 import os;
 list = [];
 list2 = [];
+x = 0
 while True:
 	a = input();
-	if a == "END":
+	if ((a == "END1") or (a == "END2") or (a == "END")):
+		if (a == "END2"):
+			x = 1
 		break;
 	list.append(a);
 count = 0;
@@ -23,11 +26,12 @@ if sv == "Windows":
 else:
 	os.system("clear");
 count = 0;
+if (x == 1):
+	print("=== " + (list[0])[12:-2] + "@zh.wikipedia ===\n{{CU request\n |status          = <!--don't change this line-->\n |language code   = zh\n |project shortcut = w\n");
 while True:
 	print(list2[count]);
 	count += 1;
 	if(count > len(list) - 1):
 		break;
-
-
-		
+if x == 1:
+	print(" |discussion      = [[:w:zh:Wikipedia:元維基用戶查核請求#" + (list[0])[12:-2] + "]]\n |reason =\n}}");
